0cfe836e1853d30d75cfa2151d34c37b84249ad9
Emmanuel Onwuegbusi <34986076+emmakodes@users.noreply.github.com>
2023-01-30 00:46:47 +0100

    Update models.py

--- wafi_djangorestframework/models.py
+++ wafi_djangorestframework/models.py
@@ -1,11 +1,6 @@
 from django.db import models
+from rest_framework import serializers, viewsets
 
 class User(models.Model):
     name = models.CharField(max_length=255)
-    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-
-class Transaction(models.Model):
-    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='transactions_sent')
-    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='transactions_received')
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
-    timestamp = models.DateTimeField(auto_now_add=True)
+    balance = models.FloatField(default=0)
